437bbc7bcf317d3105aa89008c79c48469d331d4
Nikhil Das Karavatt <karavattnikhildas@gmail.com>
2024-05-21 23:02:27 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,38 +1,44 @@
 from flask import Flask, request, render_template
+import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 app = application
 
-# Route for home page
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/predictdata', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
-    else:        
+    else:
+        form_data = {
+            'gender': request.form.get('gender'),
+            'ethnicity': request.form.get('ethnicity'),
+            'parental_level_of_education': request.form.get('parental_level_of_education'),
+            'lunch': request.form.get('lunch'),
+            'test_preparation_course': request.form.get('test_preparation_course'),
+            'reading_score': request.form.get('reading_score'),
+            'writing_score': request.form.get('writing_score')
+        }
         data = CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=int(request.form.get('reading_score')),
-            writing_score=int(request.form.get('writing_score'))
+            gender=form_data['gender'],
+            race_ethnicity=form_data['ethnicity'],
+            parental_level_of_education=form_data['parental_level_of_education'],
+            lunch=form_data['lunch'],
+            test_preparation_course=form_data['test_preparation_course'],
+            reading_score=int(form_data['reading_score']),
+            writing_score=int(form_data['writing_score'])
         )
-        
         pred_df = data.get_data_as_data_frame()
-        print("Dataframe created:", pred_df)
-        
+        print(pred_df)
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
-        print("Prediction results:", results)
-        
-        return render_template('home.html', results=round(results[0],0))
+        if results[0]> 100:
+            results[0] = 100
+        elif results[0]< 0:
+            results[0] = 0
+        return render_template('home.html', results=round(results[0]), form_data=form_data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
